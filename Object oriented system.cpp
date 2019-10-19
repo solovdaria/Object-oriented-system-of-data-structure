21f15da3345b77788a30eb780d9664f908d73d5e
solovdaria <44465483+solovdaria@users.noreply.github.com>
2019-10-19 18:52:40 +0300

    Add files via upload
    Документація: 
    file:///C:/Users/user/source/repos/Object%20oriented%20system/Object%20oriented%20system/documentation/html/index.html

--- Object oriented system.cpp
+++ Object oriented system.cpp
@@ -1,331 +1,51 @@
 ﻿// Object oriented system.cpp 
 
-/*Лабораторна робота №1
 
-Реалізувати у вигляді об’єктно-орієнтованої системи структури даних та
-алгоритми з інших предметів (Список 1). Реалізовані структури даних мають
-підтримувати роботу з даними різних типів. Продемонструвати роботу
-системи на примітивних типах (int, double), бібліотечних типах (string,
-vector), а також реалізованих класах (Список 2). Для всіх структур даних
-реалізувати методи отримання текстового подання (наприклад, з метою
-виводу на екран), а також генерації відповідних структур даних.
-6. (**) Дерева з довільною кількістю дітей (на основі зв’язного списку та
-бібліотечних засобів для списку), бінарні дерева, дерева бінарного
-пошуку. Додавання елементів, видалення елементів (за батьківським
-вузлом та індексом, за значенням), пошук за значенням.
-a. +* за різні варіанти обходу дерев, пошук за шляхом в дереві;
-b. +* за підтримку різних механізмів видалення, що якось
-обробляють елементи з видалених під-дерев.
-6. (***) «Нечесні» гральні кості з кількістю граней N=2, 4, 6, 8, 10, 12, 20.
-Грані помічено числами від 1 до N. Для кожної грані задається
-ймовірність випадіння (число від 0 до 1). Обчислення всіх можливих
-сум для заданого набору костей(набір може містити довільну кількість
-костей з однаковими чи різними гранями та однаковими чи різними
-ймовірностями) та ймовірність випадіння кожної з них.
-a. +* порівняння двох наборів за очікуваною сумою значень граней.*/
-
-#include "pch.h"
 #include <iostream>
-using namespace std;
+#include "pch.h"
+#include "Dice.h"
+#include "MyTree.h"
 
-template <typename type, typename type1>
-class MyTree
-{
-private:
-	class node
-	{
-	public:
-		node()
-		{
-			this->right = nullptr;
-			this->left = nullptr;
-		}
-		type index;
-		type1 info;
-		node*right;
-		node*left;
-	};
-	node*tree;
-public:
-	node*GetTree()
-	{
-		return tree;
-	}
+/*!
+\brief Menu provides easy and fast work
 
-	MyTree()
-	{
-		tree = nullptr;
-	}
-
-	MyTree(const MyTree &copy)
-	{
-		this->tree = copy.tree;
-	}
-
-	void Input(node*curr,type i, type1 a) //ввод дерева
-	{
-		if (tree == nullptr)
-		{
-			tree = new node();
-			tree->index = i;
-			tree->info = a;
-			tree->left = tree->right = nullptr;
-		}
-
-		if (curr == nullptr)
-		{
-			curr = new node();
-			curr->index = i;
-			curr->info = a;
-			curr->left = curr->right = nullptr;
-		}
-		if (a < curr->info)
-			if (curr->left != nullptr) Input(curr->left, i, a);
-			else
-			{
-				curr->left = new node();
-				curr->left->left = curr->left->right = nullptr;
-				curr->left->index = i;
-				curr->left->info = a;
-			}
-		if (a > curr->info)
-			if (curr->right != nullptr) Input(curr->right, i, a);
-			else
-			{
-				curr->right = new node();
-				curr->right->left = curr->right->right = nullptr;
-				curr->right->index = i;
-				curr->right->info = a;
-			}
-	} 
-
-	void Output(node*curr) //вывод значений дерева в столбец
-	{
-		if (curr != nullptr)
-		{
-			Output(curr->left);
-			cout <<"("<< curr->index+1<<" : "<< curr->info<<")"<<endl;
-			Output(curr->right);
-		}
-	}
-
-	void PrintTree(node*curr, int level) //вывод дерева в виде дерева
-	{
-		if (curr)
-		{
-			PrintTree(curr->right, level + 1);
-			for (int i = 0; i < level; i++) cout << "         ";
-			cout << "(" << curr->index + 2 << ":" << curr->info << ")" << endl;
-			PrintTree(curr->left, level + 1);
-		}
-	}
-
-	int SearchRecursion(node*curr, type1 v, int counter=1) //поиск елемента в дереве с помощью рекурсии
-	{
-
-		if (curr == NULL) return -1;
-		int tmp = curr->info * 1000;
-		double value;		
-		if (tmp % 10 > 5)
-			value = double((tmp + 1) / 1000.0);
-		else
-			value = double(tmp / 1000.0);
-		if (value == v) return counter;
-		if (v <= value)
-		{
-			if (curr->left != NULL)
-				return Search(curr->left, v, ++counter);
-			else return -1;
-		}
-		else
-		{
-			if (v > value)
-			{
-				if (curr->right != NULL)
-					return Search(curr->right, v, ++counter);
-				else return -1;
-			}
-		}
-	}
-
-	int SearchLoop(node* curr, type1 v, int counter = 1) //поиск елемента с помощью цикла
-	{
-		while (curr != nullptr)
-		{
-			int tmp = curr->info * 1000;
-			double value;
-			if (tmp % 10 > 5)
-				value = double((tmp + 1) / 1000.0);
-			else
-				value = double(tmp / 1000.0);
-			if (value == v)
-				return counter;
-			counter++;
-			curr = value > v ? curr->left : curr->right;
-		}
-		return -1;
-	}
-
-	node*Min(node*curr) //поиск минимального значения в дереве для функции удаления узла
-	{
-		node*c = curr;
-		while (c && c->left != nullptr) c = c->left;
-		return c;
-	}
-
-	int MinOutput(node*curr) //поиск минимального значения в дереве для вывода
-	{
-		node*c = curr;
-		while (c && c->left != nullptr) c = c->left;
-		return c->index+2;
-	}
-
-	node* DeleteNode(node* curr, type1 v) //удаление введенного узла в дереве
-	{
-		if (curr == nullptr)
-			return curr;
-		int tmp_ = curr->info * 1000;
-		double value;
-		if (tmp_ % 10 > 5)
-			value = double((tmp_ + 1) / 1000.0);
-		else
-			value = double(tmp_ / 1000.0);
-		if (v < value)
-			curr->left = DeleteNode(curr->left, v);
-		else if (v > value)
-			curr->right = DeleteNode(curr->right, v);
-		else
-		{
-			if (curr->left == nullptr)
-			{
-				node* tmp = curr->right;
-				free(curr);
-				return tmp;
-			}
-			else if (curr->right == nullptr)
-			{
-				node* tmp = curr->left;
-				free(curr);
-				return tmp;
-			}
-			node* tmp = Min(curr->right);
-			curr->info = tmp->info;
-			curr->index = tmp->index;
-			curr->right = DeleteNode(curr->right, tmp->info);
-		}
-		return curr;
-	}
-};
-
-class Dice
-{
-private:
-	int maxSum; 
-	int size;
-	int sum;
-	double*arr1;
-	double*arr2;
-	double*arr_sum;
-public:
-	Dice()
-	{
-		Dice*cybic;
-	}
-
-	~Dice()
-	{
-		delete[] arr1;
-		delete[] arr2;
-		delete[] arr_sum;
-	}
-
-	int AllSum(int N1, int N2) //поиск найбольшей возможной суммы на костях
-	{
-		maxSum = N1 + N2;
-		return maxSum;
-	}
-	
-	void SumArr(int size1, int size2) //третий массив, созданный из двух массивов с вероятностями хранит вероятности для всех сумм на костях
-	{
-		size = size1 + size2 - 1;
-		arr_sum = new double[size];
-		for (sum = 0; sum < size; sum++) arr_sum[sum] = 0;
-		for (sum = 0; sum < size; sum++)
-		{
-			for (int i = 0; i <= sum && i < size1; i++)
-			{
-				for (int j = 0; j <= sum && j < size2; j++)
-				{
-					if (sum == i + j) arr_sum[sum] += arr1[i] * arr2[j];
-				}
-			}
-		}
-		for (sum = 0; sum < size; sum++)
-		{
-			cout << "Probability of summ " << sum + 2 << " = " << arr_sum[sum] << endl;
-		}
-		for (int i = 0; i < size1; i++)
-		{
-			for (int j = 0; j < size2; j++)
-			{
-				if (i == j)
-				{
-					if (arr1[i] > arr2[j]) cout << "Probability of " << i + 1 << " on first > than probability of " << j+1<<" on second"<<endl;
-					else if (arr1[i] < arr2[j]) cout << "Probability of " << i + 1 << " on first < than probability of " << j+1 << " on second" <<endl;
-					else cout << "Probability of " << i + 1 << " on first = probability of " << j+1 << " on second" <<endl;
-				}
-			}
-		}
-	}
-
-	void Arrays(int N1, int N2) //ввод двух массивов, в которых хранятся значения вероятностей у двух кубиков
-	{
-		double summa = 0;
-		arr1 = new double[N1];
-		arr2 = new double[N2];
-		for (int i = 0; i < N1 - 1; i++)
-		{
-			cout << "Enter probability of " << i + 1 << ": ";
-			cin >> arr1[i];
-			summa += arr1[i];
-		}
-		arr1[N1 - 1] = 1 - summa;
-		cout << "Probability of " << N1 << " = " << arr1[N1 - 1] << endl;
-		summa = 0;
-		for (int j = 0; j < N2 - 1; j++)
-		{
-			cout << "Enter probability of " << j + 1 << ": ";
-			cin >> arr2[j];
-			summa += arr2[j];
-		}
-		arr2[N2 - 1] = 1 - summa;
-		cout << "Probability of " << N2 << " = " << arr2[N2 - 1] << endl;
-		SumArr(N1, N2);
-	}
-
-	void SumToTree(MyTree<int, double>* my, Dice cybic, int N1, int N2) //добавления массива вероятностей всех сумм в дерево
-	{
-		for (int k = 0; k < N1+N2-1; k++)
-		{
-			my->Input(my->GetTree(), k, cybic.arr_sum[k]);
-		}
-	}
-};
-
+Includes such opportunities as:
+1. Enter tree, get probabilities of all amounts and show the tree in the tree form
+2. Show all possible amounts
+3. Delete node
+4. Show minimal value of the tree
+5. Find value non-recursive
+6. Show the tree in the tree form
+\param my Is a variable of class MyTree
+\param cybic Is a variable of class Dice
+\paran N1 Maximal value on the first dice
+\param N2 Maximal value on the second dice
+\throw system("pause") In case N1 or N2 > 20 or <2
+*/
 void Menu(MyTree<int, double> my, Dice cybic)
 {
 	int N1, N2;
 	cout << "Input max number on dice:" << endl;
 	cout << "Enter N1: ";
 	cin >> N1;
+	if ((N1 > 20) || (N1 < 2))
+	{
+		cout << "Max number must be  bigger than 1 and smaller than 21" << endl;
+		system("pause");
+	}
 	cout << endl;
 	cout << "Enter N2: ";
 	cin >> N2;
+	if ((N2 > 20) || (N2 < 2))
+	{
+		cout << "Max number must be  bigger than 1 and smaller than 21" << endl;
+		system("pause");
+	}
 	cout << endl;
 	while (true)
 	{
 		system("cls");
-		cout << "MENU:\n1)Enter tree, get probabilities of all amounts and show the tree\n2)Show all possible amounts\n3)Delete node\n4)Show minimum value of the tree\n5)Find value\n6)Show the tree\n";
+		cout << "MENU:\n1)Enter tree, get probabilities of all amounts and show the tree\n2)Show all possible amounts\n3)Delete node\n4)Show minimal value of the tree\n5)Find value\n6)Show the tree\n";
 		int choice;
 		cin >> choice;
 		switch (choice)
@@ -351,7 +71,7 @@ void Menu(MyTree<int, double> my, Dice cybic)
 			int level;
 			cybic.Arrays(N1, N2);
 			cybic.SumToTree(&my, cybic, N1, N2);
-			my.PrintTree(my.GetTree(), level=0);
+			my.PrintTree(my.GetTree(), level = 0);
 			cout << "0)Back\n";
 			cin >> choice;
 			if (choice == 2)
@@ -387,7 +107,7 @@ void Menu(MyTree<int, double> my, Dice cybic)
 			double a; int result, c;
 			cout << "Enter value (probability) to find: ";
 			cin >> a;
-			result=my.SearchLoop(my.GetTree(), a, c=1);
+			result = my.SearchLoop(my.GetTree(), a, c = 1);
 			cout << result;
 			cout << "\n0)Back\n";
 			cin >> choice;
@@ -399,7 +119,7 @@ void Menu(MyTree<int, double> my, Dice cybic)
 		case 6:
 			system("cls");
 			int l;
-			my.PrintTree(my.GetTree(), l=0);
+			my.PrintTree(my.GetTree(), l = 0);
 			cout << "\n0)Back\n";
 			cin >> choice;
 			if (choice == 0)
@@ -421,4 +141,3 @@ int main()
 	Menu(my, cybic);
 	system("pause");
 }
-
